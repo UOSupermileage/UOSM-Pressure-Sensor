fe2b9878f532b2961abbcc77f97e3b9fab677d4a
Tristan Robichaud <tristan.robichaud@outlook.com>
2023-11-29 18:23:42 -0500

    fix 2 project in one

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /mnt/c/Users/trist/Documents/Projects/UOSM/UOSM-pressure-sensor
+# Install script for directory: C:/Users/trist/Documents/Projects/CLionProjects/psuosm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/UOSM-pressure-sensor")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/trist/Documents/Projects/UOSM/UOSM-pressure-sensor/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/trist/Documents/Projects/CLionProjects/psuosm/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
